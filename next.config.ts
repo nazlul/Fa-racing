e8747d2fc844166afc69b7134366d022c4707d98
Nazlul Rizan <eng.nazrizan@gmail.com>
2025-07-28 12:23:54 +0530

    Configure redirect to new hosted manifest URL

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/01982bca-3c94-fb3e-3a5c-b325f6fa1961',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
