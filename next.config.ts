d81aa9d3dc6bc4bde006ade34f64c69669a4ae2f
Nazlul Rizan <eng.nazrizan@gmail.com>
2025-07-28 11:52:23 +0530

    updated config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/01984fab-10ca-1406-4ed2-048e284ef9cd',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
